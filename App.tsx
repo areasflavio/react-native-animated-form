f59509ab46b969b74a9a403f124eb1e886bf5f9d
Flávio Arêas <vflavio9@gmail.com>
2023-04-30 13:15:14 -0300

    chore: set up native-base

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
+
+import { SignUp } from './src/screens/SignUp';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <NativeBaseProvider>
       <StatusBar style="auto" />
-    </View>
+      <SignUp />
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/SignUp.tsx
@@ -0,0 +1,11 @@
+import { Center, Heading, VStack } from 'native-base';
+
+export function SignUp() {
+  return (
+    <VStack flex={1} bgColor={'gray.300'}>
+      <Center>
+        <Heading my={24}>SignUp</Heading>
+      </Center>
+    </VStack>
+  );
+}
