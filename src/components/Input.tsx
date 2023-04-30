fa30fe806cb5b635893a756d9311cebfce2f2201
Flávio Arêas <vflavio9@gmail.com>
2023-04-30 15:31:26 -0300

    feat: create placeholder/label animation

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,5 +1,14 @@
-import { FormControl, IInputProps, Input as NBInput } from 'native-base';
-import React from 'react';
+import { FormControl, IInputProps, Input as NBInput, Text } from 'native-base';
+import React, { useEffect } from 'react';
+import Animated, {
+  Extrapolate,
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
+
+const AnimatedText = Animated.createAnimatedComponent(Text);
 
 interface InputProps extends IInputProps {
   errorMessage?: string;
@@ -9,13 +18,43 @@ export function Input({
   errorMessage = undefined,
   isInvalid,
   value,
+  placeholder,
   ...rest
 }: InputProps) {
   const isInputInvalid = !!errorMessage || isInvalid;
   const isFilled = !!value && !errorMessage;
 
+  const labelPositionY = useSharedValue(0);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    const fontSizeAnimation = interpolate(
+      labelPositionY.value,
+      [20, -20],
+      [16, 12],
+      Extrapolate.CLAMP
+    );
+
+    return {
+      top: labelPositionY.value,
+      fontSize: fontSizeAnimation,
+    };
+  });
+
+  useEffect(() => {
+    labelPositionY.value = withSpring(value || isFilled ? -20 : 20, {});
+  }, [value]);
+
   return (
-    <FormControl mb={4} isInvalid={isInputInvalid}>
+    <FormControl marginY={4} isInvalid={isInputInvalid}>
+      <AnimatedText
+        position={'absolute'}
+        color={'gray.400'}
+        left={3}
+        zIndex={1}
+        style={animatedStyle}
+      >
+        {placeholder}
+      </AnimatedText>
       <NBInput
         bg={'gray.100'}
         fontSize={'md'}
@@ -24,13 +63,15 @@ export function Input({
         borderWidth={1}
         borderColor={isFilled ? 'green.500' : 'gray.100'}
         _focus={{
-          bg: 'gray.200',
-          borderColor: 'green.500',
+          bg: 'gray.100',
+          borderColor: 'gray.500',
         }}
         _invalid={{
           borderColor: 'red.500',
         }}
         value={value}
+        placeholder={''}
+        placeholderTextColor={'gray.400'}
         {...rest}
       />
       <FormControl.ErrorMessage>{errorMessage}</FormControl.ErrorMessage>
